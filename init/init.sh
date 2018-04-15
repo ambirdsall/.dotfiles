4e9005ef6479dbdb0230ef8017e8860868c35e40
ambirdsall <alex.birdsall@sigfig.com>
2018-04-14 23:08:07 -0700

    Fill out init.sh

--- init/init.sh
+++ init/init.sh
@@ -1,5 +1,25 @@
 # usage: run from in this here directory
 # have homebrew installed first
 # maybe alias brew=apt-get or whatever if not on mac?
-./homebrew.sh
-ln -s ../bin ~/bin
+sh ./homebrew.sh
+mkdir ~/bin
+ln ../bin/* ~/bin
+
+# TODO link all rc-type files
+sh ./osx
+
+ln pryrc ~/.pryrc
+ln gitmessage ~/.gitmessage
+ln tmux.conf ~/tmux..conf
+ln vimrc ~/.vimrc
+ln zshrc ~/.zshrc
+ln gitconfig ~/.gitconfig
+ln zshenv ~/.zshenv
+ln xvimrc ~/.xvimrc
+ln railsrc ~/.railsrc
+ln ackrc ~/.ackrc
+ln rails_template ~/rails_.template
+ln spacemacs ~/.spacemacs
+ln lynxrc ~/.lynxrc
+ln inputrc ~/.inputrc
+ln gvimrc ~/.gvimrc
